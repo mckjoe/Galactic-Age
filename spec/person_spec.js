ea0f209977dedfd0fcffd54178ee8d6360ae1c88
joe mckinney <mckinney.a.joe@gmail.com>
2018-04-22 13:42:48 -0700

    add first test to check if object has been instantiated, fail.

---
+++ spec/person_spec.js
@@ -0,0 +1,15 @@
+describe('Person', function() {
+  let newPerson;
+
+  beforeEach(function() {
+    newPerson = new Person;
+  });
+
+  it('should find todays date, birth date, an age in years, and a life expectency.', function() {
+    expect(newPerson.todaysDate).toEqual(new Date());
+    expect(newPerson.birthDate).toEqual(new Date('december 9, 1986'));
+    expect(newPerson.ageInYears).toEqual(32);
+  });
+
+
+});
