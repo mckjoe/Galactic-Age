93103d4b4dd3b6f19c648fe0482b13143692302c
joe mckinney <mckinney.a.joe@gmail.com>
2018-04-22 14:13:19 -0700

    update src/personLogic.js to add a prototype for age in seconds, pass

--- src/personLogic.js
+++ src/personLogic.js
@@ -5,5 +5,9 @@ class Person {
     this.ageInYears = this.todaysDate.getYear() - this.birthDate.getYear();
     this.lifeExpectency = 77; //77 is the avg life expectency for people in the US
   }
+
+  ageInSeconds() {
+    return this.ageInYears * 31536000;
+  }
 }
 export { Person };
